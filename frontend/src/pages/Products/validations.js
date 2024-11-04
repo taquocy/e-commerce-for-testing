c5681cefd9c98ae6522e6586822d0aa25b81acb1
bao238 <dinhbao2525@gmail.com>
2024-11-04 16:22:00 +0700

    Create-New-Products-Price

--- frontend/src/pages/Products/validations.js
+++ frontend/src/pages/Products/validations.js
@@ -1,9 +1,15 @@
-import * as yup from "yup";
+import * as Yup from "yup";
 
-const editScheme = yup.object().shape({
-  title: yup.string().required(),
-  description: yup.string().min(5).required(),
-  price: yup.string().required(),
+const editScheme = Yup.object().shape({
+  title: Yup.string().required("Title is required"),
+  description: Yup.string()
+    .min(5, "Description must be at least 5 characters")
+    .required("Description is required"),
+  price: Yup.number()
+    .moreThan(0, "Price must be greater than 0")
+    .max(999999999, "Price must not exceed 9 digits")
+    .required("Price is required"),
+  photos: Yup.array().of(Yup.string().url("Must be a valid URL")),
 });
 
 export default editScheme;
