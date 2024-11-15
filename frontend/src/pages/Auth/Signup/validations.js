d27763a0798063648dcb377655d06ade3de36b38
onhaancom <ihateyoupass@gmail.com>
2024-11-15 16:47:37 +0700

    fix một số phần trong sign up
    Signup -> Sign up
    + Đề xuất mật khẩu mã hóa
    + Đánh giá mật khẩu mạnh đến yếu
    + Yêu cầu mật khẩu có ký tự đặc biệt, chữ hoa
    + Xem được mật khẩu

--- frontend/src/pages/Auth/Signup/validations.js
+++ frontend/src/pages/Auth/Signup/validations.js
@@ -1,15 +1,24 @@
 import * as yup from "yup";
 
 const validations = yup.object().shape({
-  email: yup.string().email("Nhập email hợp lệ").required("Bắt buộc"),
+  email: yup
+    .string()
+    .email("Vui lòng nhập một email hợp lệ.")
+    .required("Email là bắt buộc."),
+  
   password: yup
     .string()
-    // .min(10, "Parolanız en az 10 karakter olmalıdır.")
-    .required(),
+    .required("Mật khẩu là bắt buộc.")
+    .min(8, "Mật khẩu phải có ít nhất 8 ký tự.")
+    .matches(/[A-Z]/, "Mật khẩu phải chứa ít nhất một chữ cái viết hoa.")
+    .matches(/[a-z]/, "Mật khẩu phải chứa ít nhất một chữ cái viết thường.")
+    .matches(/[0-9]/, "Mật khẩu phải chứa ít nhất một số.")
+    .matches(/[@$!%*?&#]/, "Mật khẩu phải chứa ít nhất một ký tự đặc biệt."),
+  
   passwordConfirm: yup
     .string()
-    // .oneOf([yup.ref("password")], "Parolanız uyuşmuyor")
-    .required(),
+    .required("Xác nhận mật khẩu là bắt buộc.")
+    .oneOf([yup.ref("password")], "Mật khẩu xác nhận không khớp."),
 });
 
 export default validations;
