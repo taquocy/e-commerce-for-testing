04d9240ef70a75e4392ab74feb817237fc3fb417
phuoc2234 <nguyenvuphuoc264@gmail.com>
2024-11-18 14:11:05 +0700

    Create_Show_Password

--- frontend/src/pages/Auth/Signup/index.js
+++ frontend/src/pages/Auth/Signup/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Flex,
   Box,
@@ -7,7 +7,10 @@ import {
   FormLabel,
   Input,
   Button,
+  InputGroup,
+  InputRightElement,
   Alert,
+  FormErrorMessage,
 } from "@chakra-ui/react";
 import { useFormik } from "formik";
 import validationSchema from "./validations";
@@ -16,6 +19,10 @@ import { useAuth } from "../../../contexts/AuthContext";
 
 function Signup({ history }) {
   const { login } = useAuth();
+
+  // State để quản lý hiển thị mật khẩu
+  const [showPassword, setShowPassword] = useState(false);
+  const [showPasswordConfirm, setShowPasswordConfirm] = useState(false);
 
   const formik = useFormik({
     initialValues: {
@@ -37,12 +44,13 @@ function Signup({ history }) {
       }
     },
   });
+
   return (
     <div>
       <Flex align="center" width="full" justifyContent="center">
         <Box pt={10}>
           <Box textAlign="center">
-            <Heading>Signup</Heading>
+            <Heading>Sign Up</Heading>
           </Box>
           <Box my={5}>
             {formik.errors.general && (
@@ -51,42 +59,65 @@ function Signup({ history }) {
           </Box>
           <Box my={5} textAlign="left">
             <form onSubmit={formik.handleSubmit}>
-              <FormControl>
-                <FormLabel>E-mail</FormLabel>
+              <FormControl isInvalid={formik.touched.email && formik.errors.email}>
+                <FormLabel>
+                  E-mail <span style={{ color: "red" }}>*</span>
+                </FormLabel>
                 <Input
                   name="email"
                   onChange={formik.handleChange}
                   onBlur={formik.handleBlur}
                   value={formik.values.email}
-                  isInvalid={formik.touched.email && formik.errors.email}
                 />
+                {formik.touched.email && formik.errors.email && (
+                  <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
+                )}
               </FormControl>
 
-              <FormControl mt="4">
-                <FormLabel>Password</FormLabel>
-                <Input
-                  name="password"
-                  type="password"
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  value={formik.values.password}
-                  isInvalid={formik.touched.password && formik.errors.password}
-                />
+              <FormControl mt="4" isInvalid={formik.touched.password && formik.errors.password}>
+                <FormLabel>
+                  Mật khẩu <span style={{ color: "red" }}>*</span>
+                </FormLabel>
+                <InputGroup>
+                  <Input
+                    name="password"
+                    type={showPassword ? "text" : "password"} // Hiển thị/ẩn mật khẩu
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.password}
+                  />
+                  <InputRightElement width="4.5rem">
+                    <Button h="1.75rem" size="sm" onClick={() => setShowPassword(!showPassword)}>
+                      {showPassword ? "Ẩn" : "Hiện"}
+                    </Button>
+                  </InputRightElement>
+                </InputGroup>
+                {formik.touched.password && formik.errors.password && (
+                  <FormErrorMessage>{formik.errors.password}</FormErrorMessage>
+                )}
               </FormControl>
 
-              <FormControl mt="4">
-                <FormLabel>Password Confirm</FormLabel>
-                <Input
-                  name="passwordConfirm"
-                  type="password"
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  value={formik.values.passwordConfirm}
-                  isInvalid={
-                    formik.touched.passwordConfirm &&
-                    formik.errors.passwordConfirm
-                  }
-                />
+              <FormControl mt="4" isInvalid={formik.touched.passwordConfirm && formik.errors.passwordConfirm}>
+                <FormLabel>
+                  Xác nhận mật khẩu <span style={{ color: "red" }}>*</span>
+                </FormLabel>
+                <InputGroup>
+                  <Input
+                    name="passwordConfirm"
+                    type={showPasswordConfirm ? "text" : "password"} // Hiển thị/ẩn mật khẩu xác nhận
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.passwordConfirm}
+                  />
+                  <InputRightElement width="4.5rem">
+                    <Button h="1.75rem" size="sm" onClick={() => setShowPasswordConfirm(!showPasswordConfirm)}>
+                      {showPasswordConfirm ? "Ẩn" : "Hiện"}
+                    </Button>
+                  </InputRightElement>
+                </InputGroup>
+                {formik.touched.passwordConfirm && formik.errors.passwordConfirm && (
+                  <FormErrorMessage>{formik.errors.passwordConfirm}</FormErrorMessage>
+                )}
               </FormControl>
 
               <Button mt="4" width="full" type="submit">
