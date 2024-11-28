add6d0b63645163d17ed3cb28be18d060b1f0585
khasnhnq <quockhanh31032005@gmail.com>
2024-11-28 09:29:40 +0700

    signin

--- frontend/src/pages/Auth/Signup/validations.js
+++ frontend/src/pages/Auth/Signup/validations.js
@@ -4,11 +4,11 @@ const validations = yup.object().shape({
   email: yup.string().email("Nhập email hợp lệ").required("Bắt buộc"),
   password: yup
     .string()
-    // .min(10, "Parolanız en az 10 karakter olmalıdır.")
+    .min(6, "Password must be at least 10 characters long.")
     .required(),
   passwordConfirm: yup
     .string()
-    // .oneOf([yup.ref("password")], "Parolanız uyuşmuyor")
+    .oneOf([yup.ref("password")], "Mật khẩu phải trùng nhau")
     .required(),
 });
 
