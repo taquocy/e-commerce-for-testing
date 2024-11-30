6801c8b27129590102c036b318f18590cbb056ba
Ta Quoc Y <taquocitali@gmail.com>
2024-11-30 08:09:54 +0700

    Add search input field for `title` and filter combobox for `price` in Products page
    * Import `Select` from `@chakra-ui/react`
    * Replace second `Input` with `Select` for price filter
    * Add options for price ranges in `Select` component

--- frontend/src/pages/Products/index.js
+++ frontend/src/pages/Products/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Cards from "../../components/Card";
-import { Grid, Box, Flex, Button, Input } from "@chakra-ui/react";
+import { Grid, Box, Flex, Button, Input, Select } from "@chakra-ui/react";
 import { useInfiniteQuery } from "react-query";
 import { fetchProductList } from "../../api.js";
 
@@ -45,11 +45,18 @@ function Products() {
           onChange={(e) => setSearchTitle(e.target.value)}
           mr={2}
         />
-        <Input
+        <Select
           placeholder="Filter by price"
           value={filterPrice}
           onChange={(e) => setFilterPrice(e.target.value)}
-        />
+        >
+          <option value="">All</option>
+          <option value="0-50">0-50</option>
+          <option value="51-100">51-100</option>
+          <option value="101-200">101-200</option>
+          <option value="201-500">201-500</option>
+          <option value="501-1000">501-1000</option>
+        </Select>
       </Flex>
       <div className="products">
         <Grid templateColumns="repeat(3,1fr)" gap={4}>
