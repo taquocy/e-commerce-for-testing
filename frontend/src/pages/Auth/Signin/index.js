ae17df31d90462c0439708e09234d7fe81860d74
Nguyễn Việt Anh <147018069+VietAnh-2481@users.noreply.github.com>
2024-12-05 15:07:11 +0700

    chuc nang moi

--- frontend/src/pages/Auth/Signin/index.js
+++ frontend/src/pages/Auth/Signin/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
+import "./index.css"; // Import CSS với tên chính xác
 import {
-  Flex,
   Box,
   Heading,
   FormControl,
@@ -13,125 +13,137 @@ import {
   InputGroup,
   InputRightElement,
   Link,
+  Checkbox,
 } from "@chakra-ui/react";
-import { FaEye, FaEyeSlash } from "react-icons/fa"; // Import biểu tượng từ react-icons
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 import { useFormik } from "formik";
 import validationSchema from "./validations";
 import { fetchLogin } from "../../../api";
 import { useAuth } from "../../../contexts/AuthContext";
-import { Link as RouterLink } from "react-router-dom"; // Import RouterLink từ react-router-dom : npm install react-router-dom
+import { Link as RouterLink } from "react-router-dom";
 
 function Signin({ history }) {
   const { login } = useAuth();
-  const [showErrorIndicator, setShowErrorIndicator] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
+  const [rememberMe, setRememberMe] = useState(!!localStorage.getItem("email"));
 
   const formik = useFormik({
     initialValues: {
-      email: "",
+      email: localStorage.getItem("email") || "",
       password: "",
     },
     validationSchema,
     onSubmit: async (values, bag) => {
-      if (!values.email || !values.password) {
-        setShowErrorIndicator(true);
-      } else {
-        setShowErrorIndicator(false);
-      }
-
       try {
         const loginResponse = await fetchLogin({
           email: values.email,
           password: values.password,
         });
+
+        if (rememberMe) {
+          localStorage.setItem("email", values.email);
+        } else {
+          localStorage.removeItem("email");
+        }
+
         login(loginResponse);
         history.push("/profile");
       } catch (e) {
-        bag.setErrors({ general: e.response.data.message });
+        bag.setErrors({ general: e.response?.data?.message || "Lỗi không xác định" });
       }
     },
   });
 
   return (
-    <div>
-      <Flex align="center" width="full" justifyContent="center">
-        <Box pt={10}>
-          <Box textAlign="center">
-            <Heading>Sign In</Heading>
+    <div className="container">
+      <Box className="signin-box">
+        <Box className="signin-heading">
+          <Heading>Sign In</Heading>
+        </Box>
+        {formik.errors.general && (
+          <Box className="signin-alert">
+            <Alert status="error">{formik.errors.general}</Alert>
           </Box>
-          <Box my={5}>
-            {formik.errors.general && (
-              <Alert status="error">{formik.errors.general}</Alert>
-            )}
-          </Box>
-          <Box my={5} textAlign="left">
-            <form onSubmit={formik.handleSubmit}>
-              <FormControl isInvalid={formik.touched.email && formik.errors.email}>
-                <FormLabel>
-                  E-mail
-                  {showErrorIndicator && !formik.values.email && (
-                    <Text as="span" color="red.500">
-                      *
-                    </Text>
-                  )}
-                </FormLabel>
+        )}
+        <Box className="signin-form">
+          <form onSubmit={formik.handleSubmit}>
+            {/* Email */}
+            <FormControl isInvalid={formik.touched.email && formik.errors.email}>
+              <FormLabel className="signin-label">Email</FormLabel>
+              <Input
+                className="signin-input"
+                name="email"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.email}
+              />
+              <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
+            </FormControl>
+
+            {/* Password */}
+            <FormControl mt="4" isInvalid={formik.touched.password && formik.errors.password}>
+              <FormLabel className="signin-label">Password</FormLabel>
+              <InputGroup>
                 <Input
-                  name="email"
+                  className="signin-input"
+                  name="password"
+                  type={showPassword ? "text" : "password"}
                   onChange={formik.handleChange}
                   onBlur={formik.handleBlur}
-                  value={formik.values.email}
+                  value={formik.values.password}
                 />
-                <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
-              </FormControl>
+                <InputRightElement width="4.5rem">
+                  <Button
+                    h="2rem"
+                    w="2rem"
+                    p={0}
+                    borderRadius="full"
+                    bg="gray.200"
+                    _hover={{ bg: "gray.300" }}
+                    _active={{ bg: "gray.400" }}
+                    onClick={() => setShowPassword(!showPassword)}
+                  >
+                    {showPassword ? <FaEyeSlash /> : <FaEye />}
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
+              <FormErrorMessage>{formik.errors.password}</FormErrorMessage>
+            </FormControl>
 
-              <FormControl mt="4" isInvalid={formik.touched.password && formik.errors.password}>
-                <FormLabel>
-                  Password
-                  {showErrorIndicator && !formik.values.password && (
-                    <Text as="span" color="red.500">
-                      *
-                    </Text>
-                  )}
-                </FormLabel>
-                <InputGroup>
-                  <Input
-                    name="password"
-                    type={showPassword ? "text" : "password"}
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    value={formik.values.password}
-                  />
-                  <InputRightElement width="4.5rem">
-                    <Button
-                      h="2rem"
-                      w="2rem"
-                      p={0}
-                      borderRadius="full"
-                      bg="gray.200"
-                      _hover={{ bg: "gray.300" }}
-                      _active={{ bg: "gray.400" }}
-                      onClick={() => setShowPassword(!showPassword)}
-                    >
-                      {showPassword ? <FaEyeSlash /> : <FaEye />}
-                    </Button>
-                  </InputRightElement>
-                </InputGroup>
-                <FormErrorMessage>{formik.errors.password}</FormErrorMessage>
-              </FormControl>
+            {/* Remember Me */}
+            <FormControl mt="4" className="signin-checkbox">
+              <Checkbox
+                isChecked={rememberMe}
+                onChange={(e) => setRememberMe(e.target.checked)}
+              >
+                Remember Me
+              </Checkbox>
+            </FormControl>
 
-              <Box mt={2} textAlign="right">
-                <Link as={RouterLink} to="/forgot-password" color="teal.500">
-                  Quên mật khẩu?
+            {/* Forgot Password */}
+            <Box className="signin-forgot">
+              <Link as={RouterLink} to="/forgot-password" color="teal.500">
+                Quên mật khẩu?
+              </Link>
+            </Box>
+
+            {/* Sign Up */}
+            <Box className="signin-signup">
+              <Text>
+                Chưa có tài khoản?{" "}
+                <Link as={RouterLink} to="/signup" color="blue.500" fontWeight="bold">
+                  Đăng ký tại đây
                 </Link>
-              </Box>
+              </Text>
+            </Box>
 
-              <Button mt="4" width="full" type="submit">
-                Sign In
-              </Button>
-            </form>
-          </Box>
+            {/* Submit Button */}
+            <Button className="signin-button" type="submit">
+              Sign In
+            </Button>
+          </form>
         </Box>
-      </Flex>
+      </Box>
     </div>
   );
 }
