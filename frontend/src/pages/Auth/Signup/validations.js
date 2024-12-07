7bf4b9138a8bfd2470de85378b1a06ad021d0916
onhaancom <ihateyoupass@gmail.com>
2024-12-07 07:02:14 +0700

    Update sign-up

--- frontend/src/pages/Auth/Signup/validations.js
+++ frontend/src/pages/Auth/Signup/validations.js
@@ -3,12 +3,20 @@ import * as yup from "yup";
 const validations = yup.object().shape({
   email: yup
     .string()
-    .email("Email không hợp lệ")
+    .email("Email không hợp lệ") 
     .required("Email là bắt buộc"),
+
+  username: yup
+    .string()
+    .min(3, "Tên đăng nhập phải có ít nhất 3 ký tự.")
+    .max(20, "Tên đăng nhập không quá 20 ký tự.")
+    .required("Tên đăng nhập là bắt buộc"),
+
   password: yup
     .string()
-    .min(5, "Mật khẩu phải có ít nhất 8 ký tự.")
+    .min(8, "Mật khẩu phải có ít nhất 8 ký tự.")
     .required("Mật khẩu là bắt buộc"),
+
   passwordConfirm: yup
     .string()
     .oneOf([yup.ref("password")], "Mật khẩu xác nhận không khớp")
