24347ad51e84e3fbbf33e83bc50c65ac4c8953fe
mjjyv <minhvueye363@gmail.com>
2025-05-30 11:08:01 +0700

    minor update about env

--- frontend/src/pages/Products/validations.js
+++ frontend/src/pages/Products/validations.js
@@ -31,9 +31,9 @@ import * as yup from "yup";
 
 
 const editScheme = yup.object().shape({
-  title: yup.string().required(),
-  description: yup.string().min(5).required(),
-  price: yup.string().required(),
+  // title: yup.string().required(),
+  // description: yup.string().min(5).required(),
+  // price: yup.string().required(),
   title: yup.string().min(3, "Title must be at least 3 characters").required("Title is required"),
   description: yup.string().min(5, "Description must be at least 5 characters").required("Description is required"),
   price: yup.number().required("Price is required").positive("Price must be a positive number").typeError("Price must be a valid number"),
---
+++ note.md
@@ -0,0 +1,8 @@
+galmojalma
+
+zkzqjQN1LcZG13qY
+
+203.113.165.2/32
+
+
+mongodb+srv://galmojalma:zkzqjQN1LcZG13qY@cluster0.4udbiht.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
